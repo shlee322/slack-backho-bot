06c534ed771442275c7a01e2de17a762eab97f1d
Lee Sanghyuck <shlee322@elab.kr>
2015-03-19 17:12:42 +0900

    Delpoy 모듈 초안 #3

---
+++ modules/deploy/module.js
@@ -0,0 +1,45 @@
+var request = require('request');
+var exec = require('child_process').exec;
+
+var bot = null;
+var lastSyncTime = 0;
+
+exports.init = function(bot_obj) {
+    bot = bot_obj;
+
+    bot.slack.on('message', slack_on_message);
+}
+
+function slack_on_message(message) {
+    if(message.type != 'message') return;
+
+    var channel = bot.slack.getChannelGroupOrDMByID(message.channel);
+    if(message.text && message.text.indexOf('!deploy') === 0) {
+        var time = new Date().getTime();
+        if(time-10000 < lastSyncTime) {
+            channel.send('Deploy는 너무 자주 사용할 수 없습니다.');
+            return;
+        }
+        lastSyncTime = time;
+        exec('git rev-parse --verify HEAD', function (error, oldver, stderr) {
+            channel.send('현재 버전: ' + oldver);
+            exec('git checkout', function (err, stdout, stderr) {
+                channel.send('```\n' + stdout + '\n```');
+                exec('git rev-parse --verify HEAD', function (error, newver, stderr) {
+                    if(newver != oldver) {
+                        channel.send('신규 버전: ' + newver);
+                        channel.send('서버 갱신을 시작합니다.');
+                        setTimeout(function() {
+                            // TODO : 봇 재시작 or 리로드
+                        }, 1500);
+                    } else {
+                        channel.send('가장 최신 버전입니다.');
+                        //도배 방지
+                        lastSyncTime += 30000;
+                    }
+                });
+            });
+        });
+    }
+}
+
