2ec0d7f6fde571a812208fd5f75d66e2fa932ca2
Lee Sanghyuck <shlee322@elab.kr>
2015-03-23 23:33:35 +0900

    전역일 계산기 그래프 생성 #6

--- modules/mma/module.js
+++ modules/mma/module.js
@@ -6,7 +6,7 @@ exports.init = function(bot_obj) {
     bot = bot_obj;
 
     db.serialize(function() {
-        db.run("CREATE TABLE IF NOT EXISTS mma_data (user_id TEXT PRIMARY KEY NOT NULL, date INTEGER)");
+        db.run("CREATE TABLE IF NOT EXISTS mma_data (user_id TEXT PRIMARY KEY NOT NULL, start_date INTEGER, end_date INTEGER)");
     });
 
     bot.slack.on('message', slack_on_message);
@@ -18,19 +18,26 @@ function slack_on_message(message) {
     var channel = bot.slack.getChannelGroupOrDMByID(message.channel);
     if(message.text && message.text.indexOf('!전역일 등록 ') === 0) {
         var split_text = message.text.split(' ');
-        if(split_text.length != 3) {
-             channel.send("형식: `!전역일 등록 YYYY-MM-DD`");
+        if(split_text.length != 4) {
+             channel.send("형식: `!전역일 등록 시작일(YYYY-MM-DD) 종료일(YYYY-MM-DD)`");
              return;
         }
-        var d = Date.parse(split_text[2]);
-        if(!d) {
-            channel.send("날짜 형식이 잘못되었습니다. `!전역일 등록 YYYY-MM-DD`");
+        var start_date = Date.parse(split_text[2]);
+        var end_date = Date.parse(split_text[3]);
+
+        if(!start_date || !end_date) {
+            channel.send("날짜 형식이 잘못되었습니다.");
+            return;
+        }
+
+        if(end_date <= start_date) {
+            channel.send("전역일은 복무시작일보다 먼저일 수 없습니다");
             return;
         }
 
         db.serialize(function() {
-            db.run("INSERT OR REPLACE INTO mma_data VALUES ('" + message.user + "', " + d + ");");
-            channel.send("전역일이 " + new Date(d).toISOString().slice(0,10) + "으로 설정되었습니다");
+            db.run("INSERT OR REPLACE INTO mma_data VALUES ('" + message.user + "', " + start_date + ", " + end_time + ");");
+            channel.send("전역일이 " + new Date(end_time).toISOString().slice(0,10) + "으로 설정되었습니다");
         });
         
         return;
@@ -40,12 +47,34 @@ function slack_on_message(message) {
         //TODO : 동시에 여러명 보는 것도
         db.all("SELECT * FROM mma_data WHERE user_id='" + message.user + "';", function(err, rows) {
             if(rows.length < 1) {
-                channel.send("전역일이 등록되어 있지 않습니다. `!전역일 등록 YYYY-MM-DD`");
+                channel.send("전역일이 등록되어 있지 않습니다. `!전역일 등록 시작일(YYYY-MM-DD) 종료일(YYYY-MM-DD)`");
                 return;
             }
 
-            var date = new Date(rows[0].date);
-            channel.send("전역일 : " + date.toISOString().slice(0,10) + "\n" + "===>------------------------[10%]");
+            var message = "";
+
+            var p = (new Date().getTime() - rows[0].start_date) / (rows[0].end_date - rows[0].start_date); // 현재까지 지난 날 / 복무기간
+            if(p < 0) p = 0;
+            p = p * 100;
+
+            for(var i=0; i<=100; i=i+5) {
+                if(p < i) {
+                    message += "=";
+                } else if (p > i) {
+                    message += "-";
+                } else {
+                    message += ">";
+                }
+            }
+
+            message += "[" + p + "%]";
+
+            if(p >= 100) {
+                p = "100";
+                message = "전역을 축하합니다!";
+            }
+ 
+            channel.send("[" + bot.slack.getUserByID(rows[0].user_id)  + "] " + new Date(rows[0].end_date).toISOString().slice(0,10) + "    " + message);
         });
         return;
     }
