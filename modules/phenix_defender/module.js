c0eaf40a7dd9f364c932a1744fe023fc209341ec
whih0818 <iowm0818@naver.com>
2015-03-23 14:07:38 +0900

    출력 문구 및 기본 사용자명 변경

--- modules/phenix_defender/module.js
+++ modules/phenix_defender/module.js
@@ -5,8 +5,8 @@ var messageHeader = null;
 exports.init = function(bot_obj) {
     bot = bot_obj;
     typeInfo = {
-        'message_deleted': '-삭제-',
-        'message_changed': '-수정-'
+        'message_deleted': '-삭제됨-',
+        'message_changed': '-수정됨-'
     };
     messageHeader = "[주작질경보]";
 
@@ -19,7 +19,7 @@ function slack_on_message(message) {
     
     var subType = message.subtype;
     
-    var userName = "확인불가";
+    var userName = "알 수 없음.";
     if ("message" in message) {
         var internalMessage = message.message;
         if("edited" in internalMessage) {
@@ -31,7 +31,7 @@ function slack_on_message(message) {
     }
     
     var typeName = typeInfo[subType];
-    var returnMessage = messageHeader + typeName + " 주작충 : " + userName;
+    var returnMessage = messageHeader + " " + typeName + " 주작충 : " + userName;
     
     bot.slack.getChannelGroupOrDMByID(message.channel).send(returnMessage);
 }
