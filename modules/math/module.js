df24912ab955a22b6934495f5f99149443ea75cc
Lee Sanghyuck <shlee322@elab.kr>
2015-03-31 13:21:20 +0900

    계산 명령어 추가

---
+++ modules/math/module.js
@@ -0,0 +1,17 @@
+var math = require('mathjs');
+
+exports.init = function(bot_obj) {
+    bot = bot_obj;
+
+    bot.slack.on('message', slack_on_message);
+}
+
+function slack_on_message(message) {
+    if(message.type != 'message') return;
+
+    var channel = bot.slack.getChannelGroupOrDMByID(message.channel);
+    if(message.text && message.text.indexOf('!계산 ') === 0) {
+        channel.send(math.eval(message.text.substring(4)));
+    }
+}
+
